d483a3d1766951ef57a15fec49bbe23695ad11e5
rithvikk1011 <rithvikk1011@gmail.com>
2026-04-09 22:46:41 +0530

    Update morse.py

--- 2/morse.py
+++ 2/morse.py
@@ -1,4 +1,5 @@
 def Morse(a):
+    # This is the basis for all the morse code conversion
     sample = {
         ".-": "A", "-...": "B", "-.-.": "C", "-..": "D",
         ".": "E", "..-.": "F", "--.": "G", "....": "H",
@@ -12,16 +13,19 @@ def Morse(a):
         "-....": "6", "--...": "7", "---..": "8",
         "----.": "9"
     }
-
+    
+    # words containsthe list of words as words are split with '   '
     words = a.split('   ')
     decoded = ""
 
+    # for each word, we convert each letter back into normal text
     for word in words:
         letters = word.split(' ')
         for letter in letters:
             decoded += sample[letter]
         decoded += " "
-    
+
+    # removing the extra space added to the end
     return decoded.strip()
 
 x = input("Enter the Morse Code to be decoded: ")
